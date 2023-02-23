375b81ebe0555b8058193fd80566f039e41d41ec
seanpixel <112411911+seanpixel@users.noreply.github.com>
2023-02-22 23:59:36 -0800

    Update proof_of_concept.py

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -21,7 +21,7 @@ result = model.transcribe("./sample_lecture.mp3")
 lecture_text = result["text"]
 
 nlp = English()
-nlp.add_pipe(nlp.create_pipe('sentencizer')) # updated
+nlp.add_pipe(nlp.add_pipe('sentencizer')) 
 doc = nlp(lecture_text)
 texts = [sent.string.strip() for sent in doc.sents]
 
@@ -42,9 +42,4 @@ print("Question: ", query)
 print(chain.run(input_documents=docs, question=query))
 
 
-
-# queries = ["What aspect of West Africa languages such as Twi makes it possible for certain drums, like the atumpan, to literally speak? How are spoken and drum speech related?", "How is the term polyvocality applied in this chapter?", "Who Ladysmith Black Mambazo and what did they contribute to South African musical and socio-political history?", "How has the jeliya tradition been significant in the history of Mande society and culture?", "What cultures and music traditions of the African diaspora were mentioned in this chapter?", "Traditionally, what was the function of the Fontomfrom ensemble and its music in Akan societies? Has modernization changed this? if so, how?", "What were the six musical Africanisms listed in the chapter? How do they apply in the various musical examples explored?"]
-
-# for query in queries:
-#     print(chain.run(input_documents=docs, question=query))
 
