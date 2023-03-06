a7a0c34a60b65ec0a2238494bae1fa908852c5d1
seanpixel <seohoahn@usc.edu>
2023-03-06 15:10:56 -0800

    new v1

---
+++ frontend/src/Main.js
@@ -0,0 +1,229 @@
+import React, { useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import BarLoader from "react-spinners/BarLoader";
+import { usePromiseTracker, trackPromise } from "react-promise-tracker";
+
+const Main = () => {
+  // const object = {
+  //   summary:
+  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  //   keyterms: [
+  //     { term: "Term1", definition: "Definition1" },
+  //     { term: "Term2", definition: "Definition2" },
+  //     { term: "Term3", definition: "Definition3" },
+  //   ],
+  // };
+  const { promiseInProgress } = usePromiseTracker();
+
+  const [file, setFile] = useState(null);
+  const [studyGuide, setStudyGuide] = useState(null);
+  const [question, setQuestion] = useState("");
+  const [questionResponse, setQuestionResponse] = useState("");
+  const [link, setLink] = useState("");
+  const [showText, setShowText] = useState(false);
+
+  const toggleText = () => {
+    setShowText(!showText);
+  };
+
+  const handleFileUpload = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const uploadMp3 = async () => {
+    // event.preventDefault();
+
+    return new Promise((resolve, reject) => {
+      const mp3Data = new FormData();
+      mp3Data.append("mp3", file);
+
+      // const reader = new FileReader();
+      // reader.readAsBinaryString(file);
+      trackPromise(
+        axios
+          .post("http://0.0.0.0:4000/audio", mp3Data)
+          .then((response) => {
+            console.log("MP3 uploaded successfully");
+            console.log(response.data);
+            setStudyGuide(response.data); //may be response.data.text
+            resolve("success");
+          })
+          .catch((error) => {
+            console.error("Error uploading MP3");
+            console.error(error);
+            reject("error uploading MP3");
+          })
+      );
+    });
+  };
+  const getMp3 = async () => {
+    return new Promise((resolve, reject) => {
+      trackPromise(
+        axios
+          .post("http://0.0.0.0:4000/convert", { link: link })
+          .then((response) => {
+            console.log("MP3 uploaded successfully");
+            console.log(response.data);
+            setStudyGuide(response.data); //may be response.data.text
+            // console.log(response.data.summary.split("\n"));
+            resolve("success");
+          })
+          .catch((error) => {
+            console.error("Error retreiving youtube video");
+            console.error(error);
+            reject("error uploading MP3");
+          })
+      );
+    });
+  };
+  const askQuestion = async () => {
+    axios
+      .post("http://0.0.0.0:4000/question", {
+        question: question,
+        transcription: studyGuide.transcription,
+      }) //could be post or get
+      .then((response) => {
+        console.log("Question uploaded successfully");
+        console.log(response.data.answer);
+        setQuestionResponse(response.data.answer);
+      })
+      .catch((error) => {
+        console.error("Error retreiving question");
+        console.error(error);
+      });
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (file) {
+      const output = await uploadMp3();
+      console.log(output);
+    } else if (link) {
+      const output = await getMp3();
+      console.log(output);
+    }
+  };
+  const handleQuestion = async (event) => {
+    event.preventDefault();
+    const output = await askQuestion();
+    console.log(output);
+  };
+  return (
+    <div className="dark:bg-black">
+      <h1 className="text-6xl font-bold leading-snug mt-0 mb-2 text-teal-800 mb-0">
+        Lectura
+      </h1>
+      <p className="font-bold text-lg text-teal-700 mt-0">
+        Generate a study guide from your lecture recording!
+      </p>
+      <form
+        className="my-5 mx-auto flex flex-col w-4/5"
+        onSubmit={handleSubmit}
+      >
+        <label className="font-bold text-lg text-left text-teal-700  mb-4">
+          Upload an MP3 Lecture Recording
+        </label>
+        <div className="flex w-1/2">
+          <StyledFileSelect
+            accept=".mp3,audio/*"
+            type="file"
+            name="mp3"
+            onChange={handleFileUpload}
+            className="text-gray-400 font-bold"
+          />
+          <p className="px-3 text-3xl text-teal-800">/</p>
+          <input
+            onChange={(event) => setLink(event.target.value)}
+            className="pl-3 text-gray-400 font-bold border w-52 "
+            placeholder="paste a youtube link"
+          ></input>
+        </div>
+
+        <input
+          className="my-5 bg-teal-600 hover:bg-teal-800 text-white font-bold py-2 px-4 rounded w-1/6"
+          type="submit"
+          value="Generate"
+        />
+      </form>
+      <div className="mx-auto w-4/5">
+        {promiseInProgress && <BarLoader color="#00695c"></BarLoader>}
+        {studyGuide && (
+          <>
+            <p className="font-bold font-mono text-left">Bulletpoints:</p>
+            {studyGuide.summary
+              .replace(/Bulletpoints:/g, "")
+              .split("\n")
+              .map((item) => {
+                return (
+                  <>{item && <p className="font-mono text-left">{item}</p>}</>
+                );
+              })}
+            {/* <p className="font-mono py-7 text-left">{studyGuide.summary}</p> */}
+            <div className="my-5 flex justify-start items-start gap-5">
+              <p className="font-bold font-mono text-left">Transcription:</p>
+              <button
+                className=" bg-gray-300 hover:bg-teal-500 text-white font-bold rounded w-40"
+                onClick={toggleText}
+              >
+                Toggle Text
+              </button>
+            </div>
+
+            {showText && (
+              <p className="font-mono py-7 text-left">
+                {studyGuide.transcription}
+              </p>
+            )}
+            <p className="font-bold font-mono text-left">Key Terms:</p>
+            {/* {studyGuide.keyterms.map((element, index) => (
+              <div id={index}>
+                <p className="font-mono text-left">
+                  <span className="font-mono font-bold pr-5">
+                    {element.term}:
+                  </span>{" "}
+                  {element.definition}
+                </p>
+              </div>
+            ))} */}
+          </>
+        )}
+      </div>
+      <form
+        className="my-5 mx-auto flex flex-col w-4/5"
+        onSubmit={handleQuestion}
+      >
+        {/* <label class="text-left ">Ask a question !</label> */}
+        <input
+          accept=".mp3,audio/*"
+          type="text"
+          name="question"
+          onChange={(event) => setQuestion(event.target.value)}
+          className="text-gray-400 font-bold border w-1/5 p-3"
+          placeholder="Ask me a question !"
+        />
+        <input
+          className="my-5 bg-teal-600 hover:bg-teal-800 text-white font-bold py-2 px-4 rounded w-1/6"
+          type="submit"
+          value="Generate"
+        />
+      </form>
+      <div className="mx-auto w-4/5">
+        <p className="font-mono py-7 text-left">{questionResponse}</p>
+      </div>
+    </div>
+  );
+};
+const StyledFileSelect = styled.input`
+  border: 1px solid #00695c;
+  border-radius: 5px;
+  width: 200px;
+  ::file-selector-button {
+    font-weight: bold;
+    color: white;
+    background: #00695c;
+    padding: 0.5em;
+    border: none;
+  }
+`;
+export default Main;
+
---
+++ server.py
@@ -0,0 +1,78 @@
+from flask import Flask, jsonify, request, render_template, make_response,send_file
+from model import Model
+import model as qna
+from flask_cors import CORS
+from pytube import YouTube
+from pydub import AudioSegment
+from moviepy.editor import AudioFileClip
+from urllib.parse import urlparse, parse_qs
+import numpy as np
+import os
+import requests
+import summarize
+
+app = Flask(__name__)
+cors = CORS(app)
+
+ffmpeg_folder = '/opt/homebrew/bin'
+os.environ['PATH'] += ':' + ffmpeg_folder
+
+# cors = CORS(app, resources={"/": {"origins": "*"}})
+
+model = Model()
+
+@app.route('/audio', methods=['POST'])
+def audio():
+
+
+    # Get the audio file from the request
+    file = request.files['mp3']
+
+    # transcribe the audio and set it
+    model.setTranscription(file)
+    transcription = model.getTranscription()
+    # model.setSummary(transcription)
+    
+    summary = summarize.summarize_text(transcription)
+
+    # summarize the transcription and set it
+    # model.setSummary()
+    # summary = model.getSummary()
+
+    # Return the transcription as a JSON response
+    return jsonify({'transcription': transcription, 'summary': summary})
+
+@app.route('/question', methods=['POST'])
+def text_output():
+    # Get the text input from the form
+    data = request.get_json()
+    question = data['question']
+    transcription = data['transcription']
+
+    answer = qna.answerQuestion2(question,transcription)
+
+    # Return the processed text as a JSON response
+    return jsonify({'answer': answer})
+    
+@app.route('/convert', methods=['POST'])
+def convert():
+    data = request.get_json()
+    youtube_link = data['link']
+    # Download the YouTube video.
+    youtube_video = YouTube(youtube_link)
+    video_file = youtube_video.streams.get_highest_resolution().download()
+    # Convert the video file to MP3.
+    audio_clip = AudioFileClip(video_file)
+    audio_file = audio_clip.write_audiofile('audio.mp3')
+    # Pass the MP3 file to another API.
+    mp3_file = open('audio.mp3', 'rb')
+    files = {'mp3': mp3_file}
+    response = requests.post('http://127.0.0.1:4000/audio', files=files)
+    mp3_file.close()
+    os.remove('audio.mp3')
+    os.remove(video_file)
+    # Return the response from the other API.
+    return response.json()
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=4000)
